b0695802721151f488a7ded34e716b935055fc9f
Murat Can <44754026+SlenderShout@users.noreply.github.com>
2026-01-27 21:39:07 +0300

    hotfix

--- app/api/token/route.ts
+++ app/api/token/route.ts
@@ -2,18 +2,16 @@ import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 
 export async function POST(request: NextRequest) {
-    try {
-        const body = await request.json();
-        const { token } = body;
-
-        if (!token) {
-            return NextResponse.json({ error: "Token eksik" }, { status: 400 });
-        }
-
-        jwt.verify(token, "secret");
-        return NextResponse.json({ message: "Success" }, { status: 200 });
-        
-    } catch (error) {
-        return NextResponse.json({ error: "Geçersiz token" }, { status: 401 });
+    const { USER, PASSWORD } = process.env;
+    const { email, password } = await request.json();
+    
+    if (email === USER && password === PASSWORD) {
+        const token = jwt.sign({ email }, "secret", { expiresIn: "30d" });
+        return NextResponse.json({ token }, { status: 200 });
+    } else {
+        return NextResponse.json(
+            { error: "Hatalı kullanıcı adı veya şifre" }, 
+            { status: 401 }
+        );
     }
 }
