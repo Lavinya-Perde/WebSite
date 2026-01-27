4ac977e92df520e6b23c732488314655a58775ad
Murat Can <44754026+SlenderShout@users.noreply.github.com>
2026-01-27 21:23:39 +0300

    idk wut

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,12 +1,17 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 
-export default async function POST(request: NextRequest){
+export async function POST(request: NextRequest) {
     const { USER, PASSWORD } = process.env;
-    const { email, password } =  await request.json();
-    if(email === USER && password === PASSWORD){
-        return new Response(jwt.sign({ email }, "secret", { expiresIn: "30d" }), { status: 200 });
-    }else{
-        return new Response("Hatalı kullanıcı adı veya şifre", { status: 401 });
-    }
+    const { email, password } = await request.json();
+    
+    if (email === USER && password === PASSWORD) {
+        const token = jwt.sign({ email }, "secret", { expiresIn: "30d" });
+        return NextResponse.json({ token }, { status: 200 });
+    } else {
+        return NextResponse.json(
+            { error: "Hatalı kullanıcı adı veya şifre" }, 
+            { status: 401 }
+        );
+   }
 }
